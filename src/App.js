e0a794cd17b77551ee2a4b696122bd81fc2564de
HuuMinh1015 <129727853+HuuMinh1015@users.noreply.github.com>
2023-06-21 23:47:11 +0700

    Update bai tap react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+// import './App.css';
+
+import BaiTap from './BaiTapThucHanhLayOut';
+
+function App() {
+  return (
+    <BaiTap />
+  );
+}
+
+export default App;
---
+++ src/BaiTapThucHanhLayOut/index.js
@@ -0,0 +1,17 @@
+import Header from "./header";
+import Welcome from "./welcome";
+import Feature from "./feature";
+
+import Footer from "./footer";
+
+function BaiTap(){
+    return(
+        <div>
+            <Header />
+            <Welcome />
+            <Feature />
+            <Footer />
+        </div>
+    )
+}
+export default BaiTap;
